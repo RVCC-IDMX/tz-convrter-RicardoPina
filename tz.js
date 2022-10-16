8d78e05928e75844fe56eaf78837ab02ae9345fd
Ricardo Pina <pinazelaya@gmail.com>
2022-10-16 02:59:25 -0400

    Add first working prototype tz

--- tz.js
+++ tz.js
@@ -1,15 +1,23 @@
 const moment = require('moment-timezone');
+const yargs = require('yargs');
 
-moment.tz.setDefault('America/Los_Angeles');
+moment.tz.setDefault('America/New_York');
 
 let targetTimezone;
 
-if (process.argv.length !== 3) {
-  console.log('Usage: node <script-file><timezone>');
+console.log(yargs.argv);
+const command = yargs.argv._[0];
+const params = yargs.argv;
+
+if (yargs.argv._.length !== 1) {
+  console.log('Usage: node tz <timezone> [--format]');
   process.exit(1);
 } else {
-  // eslint-disable-next-line prefer-destructuring
-  targetTimezone = process.argv[2];
+  targetTimezone = command;
 }
 
-console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
+if (params.format) {
+  console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format('dddd, MMMM Do YYYY, h:mm:ss a')}`);
+} else {
+  console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
+}
